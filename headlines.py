96ae97ef7ae91d3c0998df8c3138cc3c78557472
CS Hui <cshui484@gmail.com>
2017-11-26 23:36:47 -0600

    able to select publication from URL

--- headlines.py
+++ headlines.py
@@ -3,11 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://www.iol.co.za/cmlink/1.640'}
+           
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
     return """<html>
     <body>
