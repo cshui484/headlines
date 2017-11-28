bdd41b002ac345a364f78534279dad990883d5a6
CS Hui <cshui484@gmail.com>
2017-11-27 23:46:15 -0600

    using Jinja object

--- headlines.py
+++ headlines.py
@@ -15,10 +15,7 @@ RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
-    return render_template('home.html',
-        title     = first_article.get('title'), 
-        published = first_article.get('published'),
-        summary   = first_article.get('summary'))
+    return render_template('home.html', article=first_article)
     
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
